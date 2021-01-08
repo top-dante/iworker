a8a52ecabef8a36bd033eeac32ef4a3475f30001
top-dante <763093576@qq.com>
2021-01-08 17:55:33 +0800

    纠结路由守卫 初步解决无限循环的梗

--- view/src/main.js
+++ view/src/main.js
@@ -1,11 +1,29 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import 'ant-design-vue/dist/antd.css'
-import './assets/common.css'
+import '@/assets/common.css'
 
-const app  = createApp(App);
+const documentTitle = 'RitaCRM-简单工作，快乐生活！';
+const whiteRouter = ['/user/login', '/user/register', '/user/recover'];
+/**
+ * 登录判断
+ */
+router.beforeEach((to, from, next) => {
+    document.title = documentTitle
+    let token = sessionStorage.getItem('token') || localStorage.getItem('token')
+    if (token) {
+        next()
+    } else {
+        if (whiteRouter.indexOf(to.path) !== -1) {
+            next()
+        } else {
+            next('/user/login')
+        }
+    }
+})
+const app = createApp(App);
 app.use(Antd);
 app.use(router);
 app.mount('#app');
